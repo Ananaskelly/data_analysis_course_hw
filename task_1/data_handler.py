d594258b432bc50f856156b323f405b3a219cb53
Ananasy <anastasiya_johnni@mail.ru>
2017-12-29 10:39:16 +0300

    отлично, работаем дальше

--- task_1/data_handler.py
+++ task_1/data_handler.py
@@ -1,17 +1,33 @@
 import numpy as np
 import scipy.io as sio
+import os
 
 
 class DataHandler:
 
     def __init__(self):
-        mat_contents = sio.loadmat('data/data_simple.mat')
+        mat_contents = sio.loadmat('task_1/data/data_simple.mat')
 
         self.X_simple = np.array(mat_contents['x']).squeeze()
         self.Y_simple = np.array(mat_contents['y']).squeeze()
+
+        mat_contents = sio.loadmat('task_1/data/data_robust.mat')
+        self.X_robust = np.array(mat_contents['x']).squeeze()
+        self.Y_robust = np.array(mat_contents['y']).squeeze()
+
+        red_wine = np.genfromtxt('task_1/data/winequality-red.csv', delimiter=';')
+        red_wine = red_wine[1:]
+        self.X_red_wine = red_wine[:, :11]
+        self.Y_red_wine = red_wine[:, 11]
 
     def get_simple_train_dataset(self):
         return self.X_simple[:40], self.Y_simple[:40]
 
     def get_simple_test_dataset(self):
         return self.X_simple[40:], self.Y_simple[40:]
+
+    def get_robust_train_dataset(self):
+        return self.X_robust[:40], self.Y_robust[:40]
+
+    def get_robust_test_dataset(self):
+        return self.X_robust[40:], self.Y_robust[40:]
--- task_1/utils.py
+++ task_1/utils.py
@@ -3,6 +3,14 @@ import numpy as np
 from scipy import linalg
 
 
+def get_x_matrix(x_init, k):
+    n = x_init.shape[0]
+    x_b = np.zeros((n, k))
+    for i in range(k):
+        x_b[:, i] = x_init ** i
+    return x_b
+
+
 def ridge(A, b, alphas):
     U, s, Vt = linalg.svd(A, full_matrices=False)
     d = s / (s[:, np.newaxis].T ** 2 + alphas[:, np.newaxis])
@@ -20,3 +28,8 @@ def HuberLoss(w, X, y):
         loss = delta * (norm_a - 1/2 * delta)
 
     return loss
+
+
+def lasso(w, alpha, X, y):
+    a = y - np.dot(X, w)
+    return np.linalg.norm(a) + np.sum(np.abs(w)) * alpha
