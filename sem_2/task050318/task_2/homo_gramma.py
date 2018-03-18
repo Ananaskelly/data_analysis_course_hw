0695047a1db4808b598f1428876be078480ff9b6
Ananasy <anastasiya_johnni@mail.ru>
2018-03-18 20:38:36 +0300

    updates

---
+++ sem_2/task050318/task_2/homo_gramma.py
@@ -0,0 +1,20 @@
+import nltk
+from nltk import Nonterminal, nonterminals, Production, CFG
+
+
+def run():
+    cool_grammar = nltk.CFG.fromstring("""
+    S -> NP VP
+    PP -> Det N
+    NP -> Det N | Det N PP | 'я'
+    VP -> V NP | VP PP
+    Det -> 'его' | 'своими'
+    N -> 'семью' | 'глазами'
+    V -> 'видел'
+    """)
+
+    sent = ['я', 'видел', 'его', 'семью', 'своими', 'глазами']
+    parser = nltk.ChartParser(cool_grammar)
+
+    for tree in parser.parse(sent):
+        print(tree)
